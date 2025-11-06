0d685c7fe237f8438ff42b5a25c2b18cc3f49fd2
Tadiyos <tadi@gmail.com>
2025-11-07 02:14:50 +0300

    edited to use 0.0.0.0 as localhost

--- app.py
+++ app.py
@@ -858,4 +858,4 @@ def clear_user_passwords_cli():
         print("Password hashes for Yob and Noon have been cleared.")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
